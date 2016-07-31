47ca0852fea32387b4e977960c94c088a1fb53af
Marco Boekholt <dhrmabo@gmail.com>
2016-07-31 03:09:05 +0200

    Added Items class
    Added a class with the items ID in it

---
+++ PokemonGo-JSON-Editor/Items.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonGo_JSON_Editor
+{
+    public class Items
+    {
+        public string UNKNOWN = "0";
+
+        public string POKEBALL = "1";
+        public string GREATBALL = "2";
+        public string ULTRABALL = "3";
+        public string MASTERBALL = "4";
+
+        public string POTION = "101";
+        public string SUPER_POTION = "102";
+        public string HYPER_POTION = "103";
+        public string MAX_POTION = "104";
+
+        public string REVIVE = "201";
+        public string MAX_REVIVE = "202";
+
+        public string LUCKY_EGG = "301";
+
+        public string INCENSE = "401";
+        public string SPICY_INCENSE = "402";
+        public string COOL_INCENSE = "403";
+        public string FLORAL_INCENSE = "404";
+
+
+        public string TROY_DISK = "501";
+
+        public string RAZZ_BERRY = "701";
+        public string BLUK_BERRY = "702";
+        public string NANAB_BERRY = "703";
+        public string WEPAR_BERRY = "704";
+        public string PINAP_BERRY = "705";
+
+    }
+}
